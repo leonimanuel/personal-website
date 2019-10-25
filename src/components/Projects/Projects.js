699dc3cde732dcf7c6f403325f20baba7a5e0bc3
LIManuel <leonmalisov@gmail.com>
2019-10-25 17:19:35 -0400

    updated grid layout

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,31 +1,55 @@
 import React from 'react';
 import './Projects.css'
-import HangmanPic from './hangman-portfolio-pic.png'
-import SmartbrainPic from './smart-brain-portfolio-pic.png'
+import HangmanPic from './hangman-pic.png'
+import SmartbrainPic from './smart-brain-pic.png'
+import AtlasPic from './atlas-pic.png'
+import PersonalWebsitePic from './personal-website-pic.png'
+
 
 const Projects = (onRouteChange) => {
 	return (
-		<div >
+		<div id="container">
 			<div style={{'font-size': '50px'}}>Projects</div>
-			<a className="project-link" href="https://hangman-clean.herokuapp.com/" target="_blank">
-				<h3>Hangman</h3>
+			<div id="projects-container">
 				<div className="project-wrapper" id="face-recognition-wrapper">
-					
+					<h3 className="project-title">Hangman</h3>
+					<p className = "project-description">Hangman is a single-page dynamically-typed website 
+					made with vanilla JS and Jquery. Includes CSS3 animations and DOM manipulation.</p>
+					<a className="project-link" href="https://hangman-clean.herokuapp.com/" target="_blank">
+						<img className="project-pic" id="hangman-pic" src={HangmanPic} alt="" width="" height=""/>
+					</a>
+				</div>
+			
+				
+				<div className="project-wrapper" id="face-recognition-wrapper">
+					<h3 className="project-title">Face Recognizer</h3>
 					<p className = "project-description">ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem 
 					ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem </p>
-					<img className="project-pic" id="hangman-pic" src={HangmanPic} alt="" width="" height=""/>
+					<a className="project-link" href="https://smart-brain-lim.herokuapp.com/" target="_blank">
+						<img className="project-pic" id="hangman-pic" src={SmartbrainPic} alt="" width="" height=""/>
+					</a>
 				</div>
-			</a>
 
-			<a className="project-link" href="https://smart-brain-lim.herokuapp.com/" target="_blank">
-				<h3>Face Recognizer</h3>
 				<div className="project-wrapper" id="face-recognition-wrapper">
-					
+					<h3 className="project-title">Atlas</h3>
 					<p className = "project-description">ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem 
 					ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem </p>
-					<img className="project-pic" id="hangman-pic" src={SmartbrainPic} alt="" width="200px" height=""/>
+					<a className="project-link" href="https://smart-brain-lim.herokuapp.com/" target="_blank">
+						<img className="project-pic" id="hangman-pic" src={AtlasPic} alt="" width="" height=""/>
+					</a>
 				</div>
-			</a>
+
+				<div className="project-wrapper" id="face-recognition-wrapper">
+					<h3 className="project-title">Squelch</h3>
+					<p className = "project-description">ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem 
+					ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem </p>
+					<a className="project-link" href="https://smart-brain-lim.herokuapp.com/" target="_blank">
+						<img className="project-pic" id="hangman-pic" src={PersonalWebsitePic} alt="" width=" " height=""/>
+					</a>
+				</div>
+
+				
+			</div>
 		</div>
 		)
 };
